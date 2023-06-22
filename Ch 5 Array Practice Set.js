77f89942dbb25964adc5250cb4059d7bcf18acba
Darshan Vasani <109815626+dpvasani@users.noreply.github.com>
2023-06-22 18:32:41 +0530

    Add files via upload

---
+++ Ch 5 Array Practice Set.js
@@ -0,0 +1,96 @@
+//                     Ch 5 Array Practice Set
+
+
+
+// Q1)How do you create an empty array in JavaScript?
+
+// Answer:
+
+// const emptyArray = [];
+
+
+// Q2)How do you check if an array is empty in JavaScript?
+
+// Answer:
+
+// const array = [];
+
+// if (array.length === 0) {
+//   console.log("The array is empty");
+// } else {
+//   console.log("The array is not empty");
+// }
+
+
+// Q3)How do you add elements to the end of an array in JavaScript?
+
+// Answer:
+
+// const array = [1, 2, 3];
+// array.push(4, 5);
+// console.log(array); // Output: [1, 2, 3, 4, 5]
+
+
+// Q4)How do you access an element at a specific index in an array?
+
+// Answer:
+
+// const array = [1, 2, 3];
+// const element = array[1];
+// console.log(element); // Output: 2
+
+
+// Q5)How do you remove the last element from an array in JavaScript?
+
+// Answer:
+
+// const array = [1, 2, 3];
+// const removedElement = array.pop();
+// console.log(array); // Output: [1, 2]
+// console.log(removedElement); // Output: 3
+
+
+// Q6)How do you find the index of a specific element in an array?
+
+// Answer:
+
+// const array = [1, 2, 3, 4, 5];
+// const index = array.indexOf(3);
+// console.log(index); // Output: 2
+
+
+// Q7)How do you concatenate two arrays in JavaScript?
+
+// Answer:
+
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const concatenatedArray = array1.concat(array2);
+// console.log(concatenatedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+
+// Q8)How do you check if an element exists in an array?
+
+// Answer:
+
+// const array = [1, 2, 3, 4, 5];
+// const elementExists = array.includes(3);
+// console.log(elementExists); // Output: true
+
+
+// Q9)How do you find the maximum value in an array?
+
+// Answer:
+
+// const array = [4, 2, 7, 5, 1];
+// const max = Math.max(...array);
+// console.log(max); // Output: 7
+
+
+// Q10)How do you reverse the order of elements in an array?
+
+// Answer:
+
+// const array = [1, 2, 3, 4, 5];
+// array.reverse();
+// console.log(array); // Output: [5, 4, 3, 2, 1]
